506cc1c8f70a75e2551fb344117e6b2546e2a5e0
dengmengmian(麻凡) <my@dengmengmian.com>
2024-06-29 13:21:08 +0800

    feat:init

---
+++ src/constant/index.js
@@ -0,0 +1,34 @@
+export const resourceType = [
+  {
+    type: '全部',
+    id: 0
+  },
+  {
+    type: '图片',
+    id: 1
+  },
+  {
+    type: '视频',
+    id: 2
+  },
+  {
+    type: '音频',
+    id: 3
+  },
+  {
+    type: '压缩包',
+    id: 4
+  },
+  {
+    type: '文档',
+    id: 5
+  },
+  {
+    type: '字体',
+    id: 6
+  },
+  {
+    type: '应用',
+    id: 7
+  }
+]
