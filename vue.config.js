bfcd20e426883b3ac15aad811a60d4409e50e7bb
zhaizhuo <zhaizhuo@dizhuodeMacBook-Pro.local>
2024-08-05 11:29:06 +0800

    fix: 修复打包问题

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,9 @@
 'use strict'
 const path = require('path')
-const defaultSettings = require('./src/settings.js')
 
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
-
-const name = defaultSettings.title || '微椒网络' // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
@@ -47,9 +44,6 @@ module.exports = {
     }
   },
   configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
-    name: name,
     resolve: {
       alias: {
         '@': resolve('src')
