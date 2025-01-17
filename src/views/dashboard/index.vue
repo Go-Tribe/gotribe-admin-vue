5d5e777a8a14222b6c7ce35d23a52f2be5a642b7
zhaizhuo <carezhzh@gmail>
2025-01-17 22:09:31 +0800

    feat:首页更新

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,40 +1,56 @@
 <template>
-  <div>
-    <el-card class="dashboard-container" shadow="always">
-      <div class="user-info">
-        <img :src="user.avatar" class="user-info-avatar">
-        <div class="user-info-right">
-          <div class="user-info-right-title">欢迎，{{ user.nickname }}，祝你开心每一天！</div>
-        </div>
-      </div>
-    </el-card>
-    <el-card class="operate-container" shadow="always">
-      <div slot="header" class="clearfix">
-        <i class="el-icon-s-operation" style="margin-right: 4px;" />
-        <span>快捷操作</span>
-      </div>
-      <div class="operate-list">
-        <router-link
-          v-for="item in operateList"
-          :key="item.name"
-          :to="item.url"
-          class="operate-item"
-        >
-          <i :class="item.icon" :style="{ color: item.color }" />
-          <span>{{ item.name }}</span>
-        </router-link>
-      </div>
-    </el-card>
+  <div class="dashboard-container">
+    <el-row>
+      <el-col v-for="item in infoList" :key="item.name" :span="6" style="padding: 0 10px;">
+        <el-card shadow="always" class="info-card">
+          <div class="info-card-top">
+            <div class="info-card-name">{{ item.name }}</div>
+            <el-tag type="success" size="mini">今日</el-tag>
+          </div>
+          <div class="info-card-value">{{ item.value }}</div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row class="mt-16">
+      <el-col v-for="item in operateList" :key="item.name" :span="4" style="padding: 0 10px;">
+        <el-card shadow="always" class="operate-card">
+          <router-link
+            :to="item.url"
+            class="operate-item"
+          >
+            <i :class="item.icon" :style="{ color: item.color }" />
+            <span>{{ item.name }}</span>
+          </router-link>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+
 export default {
   name: 'Dashboard',
   data() {
     return {
-      user: {},
+      infoList: [
+        {
+          name: '销售额',
+          value: 2
+        },
+        {
+          name: '订单量',
+          value: 2
+        },
+        {
+          name: '用户数',
+          value: 2
+        },
+        {
+          name: '新增用户',
+          value: 2
+        }
+      ],
       operateList: [
         {
           icon: 'el-icon-user-solid',
@@ -43,86 +59,72 @@ export default {
           color: 'rgb(105, 192, 255)'
         },
         {
-          icon: 'el-icon-tickets',
-          name: '文章管理',
-          url: '/content/article',
-          color: 'rgb(255, 214, 102)'
+          icon: 'el-icon-s-goods',
+          name: '商品管理',
+          url: '/store/product',
+          color: '#67c3c2'
         },
         {
-          icon: 'el-icon-s-custom',
-          name: '角色管理',
-          url: '/system/role',
-          color: 'rgb(149, 222, 100)'
+          icon: 'el-icon-s-order',
+          name: '订单管理',
+          url: '/store/order',
+          color: '#ac82e6'
+        },
+        {
+          icon: 'el-icon-document',
+          name: '文章管理',
+          url: '/content/article',
+          color: '#8ba1fa'
         },
         {
           icon: 'el-icon-menu',
-          name: '菜单管理',
-          url: '/system/menu',
+          name: '项目管理',
+          url: '/business/project',
+          color: 'rgb(149, 222, 100)'
+        },
+        {
+          icon: 'el-icon-chat-dot-square',
+          name: '评论管理',
+          url: '/operation/comment',
           color: 'rgb(255, 156, 110)'
         }
       ]
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'nickname',
-      'avatar',
-      'roles'
-    ])
-  },
-  created() {
-    this.getUser()
-  },
-  methods: {
-    getUser() {
-      this.user = {
-        nickname: this.nickname,
-        role: this.roles.join(' | '),
-        avatar: this.avatar
-      }
     }
   }
 }
 </script>
 
-<style rel="stylesheet/scss" lang="scss" scoped>
+<style lang="scss" scoped>
 .dashboard-container {
-  margin: 10px;
-  .user-info {
-    display: flex;
-    gap: 20px;
-    &-avatar {
-      height: 80px;
-      width: 80px;
-      border-radius: 50%;
-    }
-    &-right {
-      &-title {
-        font-size: 20px;
-        font-weight: 500;
+  padding: 20px 10px;
+  .info-card {
+    .info-card-top {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .info-card-name {
+        font-size: 14px;
+        color: #606266;
       }
+    }
+    .info-card-value {
+      margin-bottom: 10px;
+      font-size: 32px;
+      font-weight: 400;
+      color: #333;
+      line-height: 40px;
+      margin-top: 20px;
     }
   }
-}
-.operate-container {
-  margin: 10px;
-  .operate-list {
-    display: flex;
-    padding: 20px;
-    gap: 60px;
+  .operate-card {
+    height: 150px;
     .operate-item {
       display: flex;
-      gap: 24px;
       flex-direction: column;
       align-items: center;
-      color: #515a6e;
-      cursor: pointer;
-      span {
-        font-size: 14px;
-      }
-      &:hover span {
-        color: #57a3f3;
-      }
+      justify-content: center;
+      height: 110px;
+      gap: 20px;
     }
   }
 }
